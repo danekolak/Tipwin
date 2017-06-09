b8f92ffe84eb3b030754e646332ad922495edf7f
danijel3 <danijel3@hotmail.com>
2017-06-09 14:11:42 +0200

    PartTwo

---
+++ Tipwin/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System.Web.Mvc;
+
+namespace Tipwin.Controllers
+{
+    public class HomeController : Controller
+    {
+
+
+
+
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Tipwin/Controllers/LoginTestController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Tipwin.Controllers
+{
+    public class LoginTestController : Controller
+    {
+        // GET: LoginTest
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
